c81a39a7171342e14c7ebbf0e2fc0f030e668bee
Sarthak Roy <sarthakroy2002@gmail.com>
2022-08-14 17:48:11 +0100

    sepolicy: Change BOARD_PLAT_* to SYSTEM_EXT_*
    With AOSP 12, usage of BOARD_PLAT_* sepolicy directory specification
    has been deprecated. The recommended macro is SYSTEM_EXT_* directory
    specification. Switch to it.
    
    Signed-off-by: Sarthak Roy <sarthakroy2002@gmail.com>
    Signed-off-by: LinkBoi00 <linkdevel@protonmail.com>

--- SEPolicy.mk
+++ SEPolicy.mk
@@ -4,10 +4,10 @@ BOARD_SEPOLICY_DIRS += \
     device/mediatek/sepolicy/basic/non_plat \
     device/mediatek/sepolicy/bsp/non_plat
 
-BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
+SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += \
     device/mediatek/sepolicy/basic/plat_private \
     device/mediatek/sepolicy/bsp/plat_private
 
-BOARD_PLAT_PUBLIC_SEPOLICY_DIR += \
+SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += \
     device/mediatek/sepolicy/basic/plat_public \
     device/mediatek/sepolicy/bsp/plat_public
