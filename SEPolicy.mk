94e69231d73f7528d34b14b5d214ae44ff2f3372
bengris32 <bengris32@protonmail.ch>
2022-08-14 17:01:39 +0100

    sepolicy: Initial bringup * Rename BoardSEPolicyConfig.mk to SEPolicy.mk * Drop useless OTA upgrade sepolicy * Unconditionally include debug sepolicy
    Signed-off-by: bengris32 <bengris32@protonmail.ch>
    Change-Id: I0c43f3c4783127aad1e5f653bf12b5286cba74ed

---
+++ SEPolicy.mk
@@ -0,0 +1,18 @@
+BOARD_SEPOLICY_DIRS += \
+    device/mediatek/sepolicy/basic/non_plat \
+    device/mediatek/sepolicy/basic/debug/non_plat \
+    device/mediatek/sepolicy/bsp/non_plat \
+    device/mediatek/sepolicy/bsp/debug/non_plat \
+    device/mediatek/sepolicy/modem
+
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
+    device/mediatek/sepolicy/basic/plat_private \
+    device/mediatek/sepolicy/basic/debug/plat_private \
+    device/mediatek/sepolicy/bsp/plat_private \
+    device/mediatek/sepolicy/bsp/debug/plat_private
+
+BOARD_PLAT_PUBLIC_SEPOLICY_DIR += \
+    device/mediatek/sepolicy/basic/plat_public \
+    device/mediatek/sepolicy/basic/debug/plat_public \
+    device/mediatek/sepolicy/bsp/plat_public \
+    device/mediatek/sepolicy/bsp/debug/plat_public
